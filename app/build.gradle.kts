5b5f638432ba0d43ed1aaa200d74631d284058da
ClaudeCodeAi <97942287@qq.com>
2026-05-05 00:18:36 +0800

    chore: 升级版本至 v1.8.0 (versionCode 12)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.readtrack"
         minSdk = 34
         targetSdk = 36
-        versionCode = 11
-        versionName = "1.7.0"
+        versionCode = 12
+        versionName = "1.8.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
