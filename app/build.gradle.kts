8965ac9cb0f67241426167a357c159d95c380740
ClaudeCodeAi <97942287@qq.com>
2026-05-08 15:45:49 +0800

    chore(release): bump version to 1.9.5 (versionCode 21)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.readtrack"
         minSdk = 34
         targetSdk = 36
-        versionCode = 20
-        versionName = "1.9.0"
+        versionCode = 21
+        versionName = "1.9.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
